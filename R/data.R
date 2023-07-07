7206901ba376d84acec4bc83e2077f6e2351e1c5
Hohsuk Noh <hsnoh@sookmyung.ac.kr>
2023-07-07 12:13:10 +0900

    data update

--- R/data.R
+++ R/data.R
@@ -4,10 +4,11 @@
 #' (Non-deep simple morphophysiological dormancy in seeds of heavenly bamboo)
 #'
 #' @format ## `nandina`
-#' A data frame with 12 rows and 3 columns:
+#' A data frame with 12 rows and 4 columns:
 #' \describe{
 #'   \item{treatment}{treatment level}
 #'   \item{num_seed}{number of tested seeds}
 #'   \item{ger_seed}{number of germinated seeds}
+#'   \item{rep}{replicate number}
 #' }
 "nandina"
